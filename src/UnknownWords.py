868b19e3b50701fcab54275d10f23d5e7ee8f39f
QEDan <danpmazur@gmail.com>
2019-02-02 23:14:23 -0800

    Created UnknownWords class and tests.

---
+++ src/UnknownWords.py
@@ -0,0 +1,62 @@
+import logging
+import numpy as np
+from keras import Input, Model
+from keras.layers import Embedding as EmbeddingLayer, Bidirectional, CuDNNLSTM, Dense
+from sklearn.model_selection import train_test_split
+
+
+class UnknownWords:
+    def __init__(self, text_mapper, embedding, max_words=5000,
+                 loss='mean_squared_error'):
+        self.text_mapper = text_mapper
+        self.embedding = embedding
+        self.max_words = max_words
+        self.loss = loss
+        self.model = None
+
+    def texts_to_x(self, word):
+        """ Handles mapping one text doc into model inputs """
+        chars_x = np.zeros(self.text_mapper.max_word_len)
+
+        for char_ind, char in enumerate(word[:self.text_mapper.max_word_len]):
+            chars_x[char_ind] = self.text_mapper.char_mapper.get_symbol_index(char)
+
+        return chars_x
+
+    def define_model(self):
+        max_word_len = self.text_mapper.max_word_len
+        char_vocab_size = self.text_mapper.char_mapper.get_vocab_len()
+        chars_input = Input(shape=(max_word_len,), name='chars_input', dtype='int32')
+        m = EmbeddingLayer(input_dim=char_vocab_size,
+                           output_dim=64,
+                           input_length=max_word_len)(chars_input)
+        m = Bidirectional(CuDNNLSTM(64))(m)
+        m = Dense(self.embedding.embedding_matrix.shape[1])(m)
+        self.model = Model(inputs=chars_input, outputs=m)
+        self.model.compile(loss=self.loss, optimizer='adam', metrics=['accuracy'])
+        return self.model
+
+    def sample_training_data(self, sentences):
+        n_tokens = 0
+        words = set()
+        while n_tokens < self.max_words:
+            sample_sentence = np.random.choice(sentences)
+            words = words.union(sample_sentence.split())
+            n_tokens = len(words)
+        list_words = list(words)
+        list_tokens = [self.texts_to_x(word) for word in list_words]
+        list_embeddings = [self.embedding.embeddings_index[t] for t in list_words]
+        array_tokens = np.array(list_tokens)
+        array_embeddings = np.array(list_embeddings)
+        train_X, val_X, train_y, val_y = train_test_split(array_tokens, array_embeddings, test_size=0.2)
+        return train_X, val_X, train_y, val_y
+
+    def fit(self, sentences):
+        logging.info('Fitting UnknownWords model...')
+        train_X, val_X, train_y, val_y = self.sample_training_data(sentences)
+        self.model.fit(x=train_X, y=train_y, epochs=10, batch_size=128)
+
+    def predict(self, words):
+        input_x = np.array([self.texts_to_x(word) for word in words])
+        predictions = self.model.predict(input_x)
+        return predictions
---
+++ tests/test_UnknownWords.py
@@ -0,0 +1,56 @@
+import unittest
+import numpy as np
+import keras
+import spacy
+
+from src.UnknownWords import UnknownWords
+from src.data_mappers import TextMapper
+
+
+class TestEmbedding:
+    def __init__(self):
+        self.embeddings_index = {'aaa': np.array([1.0, 1.0, 1.0]),
+                                'aab': np.array([1.0, 1.1, 1.1]),
+                                'aac': np.array([1.0, 1.1, 1.2])}
+        self.embedding_matrix = np.array([self.embeddings_index[k] for k in self.embeddings_index.keys()])
+
+
+class TestUnknownWords(unittest.TestCase):
+    def setUp(self):
+        self.embedding = TestEmbedding()
+        self.nlp = spacy.load('en_core_web_sm', disable=['parser', 'tagger', 'ner'])
+        self.text_mapper = TextMapper(word_counts=[('aaa', 1), ('aab', 1), ('aac', 1)],
+                                      char_counts=[('a', 1), ('b', 1), ('c', 1)],
+                                      word_threshold=10, max_word_len=3,
+                                      char_threshold=350, max_sent_len=3, nlp=self.nlp, word_lowercase=True,
+                                      char_lowercase=True)
+        self.unknown_words_model = UnknownWords(self.text_mapper, self.embedding, max_words=3)
+
+    def test_define_model(self):
+        model = self.unknown_words_model.define_model()
+        self.assertTrue(type(model) == keras.Model)
+
+    def test_sample_training_data(self):
+        sentences = ['aaa aaa aab', 'aaa aab aab', 'aac aab aac']
+        train_X, val_X, train_y, val_y = self.unknown_words_model.sample_training_data(sentences)
+        self.assertGreater(len(train_X), 0)
+        self.assertGreater(len(val_X), 0)
+        self.assertGreater(len(train_y), 0)
+        self.assertGreater(len(val_y), 0)
+        self.assertEqual(train_X.shape, (2, 3))
+        self.assertEqual(val_X.shape, (1, 3))
+        self.assertEqual(val_y.shape, (1, 3))
+        self.assertEqual(train_y.shape, (2, 3))
+
+    def test_fit(self):
+        sentences = ['aaa aaa aab', 'aaa aab aab', 'aac aab aac']
+        self.unknown_words_model.define_model()
+        self.unknown_words_model.fit(sentences)
+
+    def test_predict(self):
+        sentences = ['aaa aaa aab', 'aaa aab aab', 'aac aab aac']
+        self.unknown_words_model.define_model()
+        self.unknown_words_model.fit(sentences)
+        preds = self.unknown_words_model.predict(['aad'])
+        self.assertEqual(type(preds), np.ndarray)
+        self.assertEqual(preds.shape, (1, 3))
